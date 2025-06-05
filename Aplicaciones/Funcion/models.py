a5ab271a7d3200d230323968d820e3ba51287a78
Javier Pilco <brandon.pilco3401@utc.edu.ec>
2025-06-05 11:23:20 -0500

    2 Creacion models

--- Aplicaciones/Funcion/models.py
+++ Aplicaciones/Funcion/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Cine(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    ciudad = models.CharField(max_length=100)
+    capacidad_total = models.PositiveIntegerField()
+    numero_salas = models.PositiveIntegerField()
+
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=150)
+    genero = models.CharField(max_length=50)
+    duracion = models.PositiveIntegerField(help_text="Duración en minutos")
+    fecha_funcion = models.DateField()
+    cine = models.ForeignKey(Cine, on_delete=models.CASCADE, related_name='peliculas')
