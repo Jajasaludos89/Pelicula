3bb0cd7f32b3de6066c3df20799a7d8b04686203
Javier Pilco <brandon.pilco3401@utc.edu.ec>
2025-06-05 12:41:43 -0500

    10 configuracion de los settings 2.0

--- Aplicaciones/Funcion/apps.py
+++ Aplicaciones/Funcion/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FuncionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Funcion'
+    name = 'Aplicaciones.Funcion'
